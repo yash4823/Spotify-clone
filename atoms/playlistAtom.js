144a3a7f52abc717842ce5d82ab304b2d67ffdd9
Yash goyal <65528632+yash4823@users.noreply.github.com>
2023-08-22 02:00:53 +0530

    Add files via upload

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
+
+export const playlistIdstate = atom({
+  key: "playlistIdstate",
+  default: '37i9dQZF1DZ06evO4mwRSU',
+});
